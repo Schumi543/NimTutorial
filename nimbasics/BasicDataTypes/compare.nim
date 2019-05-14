20f374209ee093f1dc7b3638f62e149988c642cc
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2019-05-14 23:13:34 +0900

    refactor

--- nimbasics/BasicDataTypes/compare.nim
+++ nimbasics/BasicDataTypes/compare.nim
@@ -1,3 +1,4 @@
-let incomeAlice : float = 400 * (30 / 15)
-let incomeBob : float= 3.14 * 8 * 30
+let
+  incomeAlice : float = 400 * (30 / 15)
+  incomeBob : float= 3.14 * 8 * 30
 echo if incomeAlice >= incomeBob : "Yes" else : "No"
